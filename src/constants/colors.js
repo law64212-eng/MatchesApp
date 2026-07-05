03ab0e13ed14b23c9c72328491fd6cf722ddc821
law64212-eng <law64212@gmail.com>
2026-07-06 02:27:19 +0300

    Create colors.js

---
+++ src/constants/colors.js
@@ -0,0 +1,15 @@
+export const COLORS = {
+  background: '#0B1120',
+  surface: '#151E30',
+  surfaceElevated: '#1C2740',
+  primary: '#D4AF37',
+  primaryDark: '#A88328',
+  accentRed: '#C8102E',
+  accentBlue: '#004D98',
+  textPrimary: '#F5F6FA',
+  textSecondary: '#9AA5B8',
+  live: '#3ECF6B',
+  border: '#26324A',
+};
+
+export default COLORS;
